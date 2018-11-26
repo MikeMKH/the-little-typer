6816c6200c50b501490e5ee130a5521512487d72
Mike Harris <MikeMKH@users.noreply.github.com>
2018-11-26 06:29:40 -0600

    added chapter 1

--- ch01/exercises.rkt
+++ ch01/exercises.rkt
@@ -1,1 +1,30 @@
 #lang pie
+
+; 81
+(claim four Nat)
+
+(define four
+  (add1
+    (add1
+      (add1
+        (add1
+          zero)))))
+
+(check-same Nat four 4)
+
+; 119
+(check-same (Pair Nat Atom)
+  (cons zero 'onion)
+  (cons
+    (car
+      (the (Pair Nat Atom)
+        (cons zero 'something-else)))
+    (cdr
+      (the (Pair Atom Atom)
+        (cons 'apple 'onion)))))
+
+; 120
+(the (Pair Atom
+       (Pair Atom Atom))
+  (cons 'basil
+    (cons 'thyme 'oregano)))
