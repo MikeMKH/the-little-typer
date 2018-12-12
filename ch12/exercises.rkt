7633094754b71e86d9ef57bd7acdc7ec6f2c0bc7
Mike Harris <MikeMKH@users.noreply.github.com>
2018-12-12 06:36:42 -0600

    added chapter 12 Even

---
+++ ch12/exercises.rkt
@@ -0,0 +1,83 @@
+#lang pie
+
+;----------------------------------
+; need + definition from chapter 3
+;----------------------------------
+
+; 3.24
+(claim +
+  (→ Nat Nat
+    Nat))
+
+; 3.27
+(define +
+  (λ (n m)
+    (iter-Nat n
+      m
+      (λ (+n-1)
+        (add1 +n-1)))))
+
+;--------------------------------------
+; need double definition from chapter 9
+;--------------------------------------
+
+; 9.21
+(claim double
+  (→ Nat
+    Nat))
+
+(define double
+  (λ (n)
+    (iter-Nat n
+      0
+      (+ 2))))
+
+; 5
+(claim Even
+  (-> Nat
+      U))
+
+(define Even
+  (lambda (n)
+    (Sigma ((half Nat))
+      (= Nat n (double half)))))
+
+; 9
+(claim zero-is-even
+  (Even 0))
+
+(define zero-is-even
+  (cons 0
+    (same 0)))
+
+(claim ten-is-even
+  (Even 10))
+
+(define ten-is-even
+  (cons 5
+    (same 10)))
+
+; 13
+(claim +two-even
+  (Pi ((n Nat))
+    (-> (Even n)
+        (Even (+ 2 n)))))
+
+; 26
+(define +two-even
+  (lambda (n e_n)
+    (cons (add1 (car e_n))
+      (cong (cdr e_n) (+ 2)))))
+
+; 28
+(claim two-is-even
+  (Even 2))
+
+(define two-is-even
+  (+two-even 0 zero-is-even))
+
+(claim four-is-even
+  (Even 4))
+
+(define four-is-even
+  (+two-even 2 two-is-even))
