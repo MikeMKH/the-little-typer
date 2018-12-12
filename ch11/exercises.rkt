38704166de4f3b7b94838f215bd110c055a1c168
Mike Harris <MikeMKH@users.noreply.github.com>
2018-12-11 18:47:25 -0600

    added list->vec->list=

--- ch11/exercises.rkt
+++ ch11/exercises.rkt
@@ -80,3 +80,195 @@
   (vec-append Atom 0 1
     vecnil (vec:: 'a vecnil))
   (vec:: 'a vecnil))
+
+; 31
+(claim mot-vec->list
+  (Pi ((E U)
+       (l Nat))
+    (-> (Vec E l)
+        U)))
+
+(define mot-vec->list
+  (lambda (E l)
+    (lambda (es)
+      (List E))))
+
+(claim step-vec->list
+  (Pi ((E U)
+       (l-1 Nat)
+       (e E)
+       (es (Vec E l-1)))
+    (-> (mot-vec->list E l-1 es)
+        (mot-vec->list E (add1 l-1) (vec:: e es)))))
+
+(define step-vec->list
+  (lambda (E l-1 e es)
+    (lambda (vec->list_es)
+      (:: e vec->list_es))))
+
+; 32
+(claim vec->list
+  (Pi ((E U)
+       (l Nat))
+    (-> (Vec E l)
+        (List E))))
+
+(define vec->list
+  (lambda (E l)
+    (lambda (es)
+      (ind-Vec l es
+        (mot-vec->list E)
+        nil
+        (step-vec->list E)))))
+
+(check-same (List Atom)
+  (vec->list Atom 0 vecnil)
+  nil)
+
+(check-same (List Nat)
+  (vec->list Nat 1 (vec:: 1 vecnil))
+  (:: 1 nil))
+
+(check-same (List Atom)
+  (vec->list Atom 1 (vec:: 'a vecnil))
+  (:: 'a nil))
+
+;-----------------------
+; length from chapter 5
+;-----------------------
+
+; 5.38
+(claim step-length
+  (Π ((E U))
+    (→ E (List E) Nat
+        Nat)))
+
+(define step-length
+  (λ (E)
+    (λ (e es length-es)
+      (add1 length-es))))
+
+; 5.36
+(claim length
+  (Π ((E U))
+    (→ (List E)
+        Nat)))
+
+; 5.39
+(define length
+  (λ (E)
+    (λ (es)
+      (rec-List es
+        0
+        (step-length E)))))
+
+;-------------------------------
+; need list->vec from chapter 10
+;-------------------------------
+
+; 10.70
+(claim mot-list->vec
+  (Π ((E U))
+    (→ (List E)
+        U)))
+
+(define mot-list->vec
+  (λ (E es)
+    (Vec E (length E es))))
+
+; 10.71
+(claim step-list->vec
+  (Π ((E U)
+       (e E)
+       (es (List E)))
+    (→ (mot-list->vec E es)
+        (mot-list->vec E (:: e es)))))
+
+; 10.72
+(define step-list->vec
+  (λ (E e es)
+    (λ (list->vec_es)
+      (vec:: e list->vec_es))))
+
+; 10.54
+(claim list->vec
+  (Π ((E U)
+       (es (List E)))
+    (Vec E (length E es))))
+
+; 10.77
+(define list->vec
+  (λ (E es)
+    (ind-List es
+      (mot-list->vec E)
+      vecnil
+      (step-list->vec E))))
+
+; 39
+(claim mot-list->vec->list=
+  (Pi ((E U))
+    (-> (List E)
+        U)))
+
+(define mot-list->vec->list=
+  (lambda (E es)
+    (= (List E)
+      es
+      (vec->list E
+        (length E es)
+        (list->vec E es)))))
+
+; 53
+(claim ::-fun
+  (Pi ((E U))
+    (-> E (List E)
+        (List E))))
+
+(define ::-fun
+  (lambda (E)
+    (lambda (e es)
+      (:: e es))))
+
+; 40
+(claim step-list->vec->list=
+  (Pi ((E U)
+       (e E)
+       (es (List E)))
+  (-> (mot-list->vec->list= E es)
+      (mot-list->vec->list= E (:: e es)))))
+
+; 54
+(define step-list->vec->list=
+  (lambda (E e es)
+    (lambda (list->vec->list=_es)
+      (cong list->vec->list=_es
+        (::-fun E e)))))
+
+; 35
+(claim list->vec->list=
+  (Pi ((E U)
+       (es (List E)))
+    (= (List E)
+       es
+       (vec->list E (length E es)
+         (list->vec E es)))))
+
+; 55
+(define list->vec->list=
+  (lambda (E es)
+    (ind-List es
+      (mot-list->vec->list= E)
+      (same nil)
+      (step-list->vec->list= E))))
+
+(check-same (= (List Nat) nil nil)
+  (list->vec->list= Nat nil)
+  (same nil))
+
+(check-same (= (List Nat) (:: 1 nil) (:: 1 nil))
+  (list->vec->list= Nat (:: 1 nil))
+  (same (:: 1 nil)))
+
+(check-same (= (List Atom) (:: 'a (:: 'b nil)) (:: 'a (:: 'b nil)))
+  (list->vec->list= Atom (:: 'a (:: 'b nil)))
+  (same (:: 'a (:: 'b nil))))
